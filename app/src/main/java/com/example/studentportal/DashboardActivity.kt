2070f0855db028e4f30499c7d334738f4c5f10a2
raihankabir1952 <raihankabir1952@gmail.com>
2025-12-06 21:45:00 +0600

    change

--- app/src/main/java/com/example/studentportal/DashboardActivity.kt
+++ app/src/main/java/com/example/studentportal/DashboardActivity.kt
@@ -95,8 +95,6 @@ class DashboardActivity : AppCompatActivity() {
         }
 
         // Logout
-//        btnLogout.setOnClickListener {
-//            finish()
         btnLogout.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
